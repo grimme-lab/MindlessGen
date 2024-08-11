a45da81dfe25e6684f05421e11b0ce42f84907c6
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-08-11 18:57:10 +0200

    switch ordering to avoid formatting files generated by setuptools_scm
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- src/mlmgen/__version__.py
+++ src/mlmgen/__version__.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev5+g40508a9.d20240811"
-__version_tuple__ = version_tuple = (0, 1, "dev5", "g40508a9.d20240811")
+__version__ = version = "0.1.dev7+gfa62e49.d20240811"
+__version_tuple__ = version_tuple = (0, 1, "dev7", "gfa62e49.d20240811")
