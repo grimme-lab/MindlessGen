452b8df5c13f70c31a7f8f128c15e9fe342d1e4f
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-08-12 10:39:42 +0200

    update gitignore and limit to Python 3.10 and higher
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- src/mlmgen/__version__.py
+++ src/mlmgen/__version__.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev8+ga45da81.d20240812"
-__version_tuple__ = version_tuple = (0, 1, "dev8", "ga45da81.d20240812")
+__version__ = version = "0.1.dev9+g0dbaecc.d20240812"
+__version_tuple__ = version_tuple = (0, 1, "dev9", "g0dbaecc.d20240812")
