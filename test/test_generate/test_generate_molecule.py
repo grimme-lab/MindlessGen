d8be3d0d9f2d3417dd2f48dca7e14ab5d80c116f
Jonathan Schöps <s6jtscho@uni-bonn.de>
2024-11-14 17:35:17 +0100

    Changes a bug with an array type
    Signed-off-by: Jonathan Schöps <s6jtscho@uni-bonn.de>

--- test/test_generate/test_generate_molecule.py
+++ test/test_generate/test_generate_molecule.py
@@ -413,7 +413,6 @@ def test_fixed_charge(
 ):
     """Test the right assinged charge when a molecular charge is given"""
     default_generate_config.molecular_charge = "3"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 5
     default_generate_config.max_num_atoms = 15
 
@@ -428,7 +427,6 @@ def test_fixed_charge_and_no_possible_correction(
 ):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "3"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 5
     default_generate_config.max_num_atoms = 5
     default_generate_config.forbidden_elements = "1-10, 12-*"
@@ -447,7 +445,6 @@ def test_fixed_charge_and_fixed_composition(
 ):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "3"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 5
     default_generate_config.max_num_atoms = 10
     default_generate_config.element_composition = "H:5-5, C:2-2, N:1-1, O:1-1"
@@ -463,7 +460,6 @@ def test_fixed_charge_and_fixed_composition(
 def test_fixed_charge_hydrogen_correction(default_generate_config):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "3"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 7
     default_generate_config.max_num_atoms = 15
     default_generate_config.element_composition = "B:1-1, Ne:2-2, P:1-1, Cl:1-1"
@@ -477,7 +473,6 @@ def test_fixed_charge_hydrogen_correction(default_generate_config):
 def test_fixed_charge_no_hydrogen_correction(default_generate_config):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "2"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 11
     default_generate_config.max_num_atoms = 15
     default_generate_config.element_composition = "H:4-4, B:1-1, Ne:2-2, P:1-1, Cl:1-1"
@@ -492,7 +487,6 @@ def test_fixed_charge_no_hydrogen_correction(default_generate_config):
 def test_fixed_charge_with_lanthanides(default_generate_config):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "3"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 7
     default_generate_config.max_num_atoms = 15
     default_generate_config.element_composition = "B:1-1, Ne:2-2, P:1-1, Cl:1-1, Lr:1-1"
@@ -506,7 +500,6 @@ def test_fixed_charge_with_lanthanides(default_generate_config):
 def test_fixed_charge_with_lanthanides_2(default_generate_config):
     """Test the hydrogen correction for a fixed charge"""
     default_generate_config.molecular_charge = "2"
-    default_generate_config.set_molecular_charge = True
     default_generate_config.min_num_atoms = 7
     default_generate_config.max_num_atoms = 15
     default_generate_config.element_composition = "B:1-1, Ne:2-2, P:1-1, Cl:1-1, Lu:1-1"
