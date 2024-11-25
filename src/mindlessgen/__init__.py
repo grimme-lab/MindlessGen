c98231daf2b44cc216da8e1ae32433a11c8327a4
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-11-25 10:01:23 +0100

    remove dummy code snippets from early development
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- src/mindlessgen/__init__.py
+++ src/mindlessgen/__init__.py
@@ -1,12 +1,7 @@
 """
-Squarer
-=======
-
-Dummy command line tool to square a number.
+Mindless molecule generator initialization module.
 """
 
-from .__version__ import __version__
 from .cli import console_entry_point
-from .generator import generator as square
 
-__all__ = ["__version__", "console_entry_point", "square"]
+__all__ = ["console_entry_point"]
