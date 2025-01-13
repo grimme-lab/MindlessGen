04d20136d2848c1d4a3b2989246b5e656cd42a1c
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2025-01-13 08:21:33 +0100

    rename files accordingly
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- src/mindlessgen/qm/__init__.py
+++ src/mindlessgen/qm/__init__.py
@@ -5,7 +5,7 @@ This module contains all QM-related functions and classes.
 from .base import QMMethod
 from .xtb import XTB, get_xtb_path
 from .orca import ORCA, get_orca_path
-from .gp3 import GXTB, get_gxtb_path
+from .gxtb import GXTB, get_gxtb_path
 
 __all__ = [
     "XTB",
